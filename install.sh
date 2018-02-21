f089051d609ceb14d3cc4178d8e245472e9b9ae0
Romain Ouabdelkader <rouabdelkader@gopro.com>
2018-02-21 14:04:20 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 set -xe
 
 mkdir -p ~/.emacs.d/straight/versions
-ln -s lock-versions.el ~/.emacs.d/straight/versions/default.el
+ln -s `pwd`/lock-versions.el ~/.emacs.d/straight/versions/default.el
