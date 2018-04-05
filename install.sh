d963a7e195cc61dd1f9d18a4f415aaafa48afbaa
Romain Ouabdelkader <rouabdelkader@gopro.com>
2018-04-05 16:16:16 +0200

    improve install script

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-set -xe
+set -e
 
 mkdir -p ~/.emacs.d/straight/versions
-ln -s `pwd`/lock-versions.el ~/.emacs.d/straight/versions/default.el
+
+lock_file=~/.emacs.d/straight/versions/default.el
+if [ ! -f ~/.emacs.d/straight/versions/default.el ]; then
+    echo "Setting up lock file.."
+    ln -s `pwd`/lock-versions.el ~/.emacs.d/straight/versions/default.el
+    echo
+fi
+
+echo "Put the following in your ~/.emacs:"
+echo "(load-file \"$(pwd)/init.el\")"
