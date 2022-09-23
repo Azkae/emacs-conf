273a899dea7f2fd942635e00616b381fa1f5589d
Romain Ouabdelkader <romain.ouabdelkader@gmail.com>
2022-09-23 14:31:38 +0200

    org-roam: hide properties

--- init.el
+++ init.el
@@ -928,7 +928,35 @@ With argument ARG, do this that many times."
    ("C-c n g" . org-roam-graph)
    :map org-mode-map
    (("C-c n i" . org-roam-node-insert))
-   (("M-," . org-mark-ring-goto))))
+   (("M-," . org-mark-ring-goto)))
+  :config
+  (defun org-hide-properties ()
+    "Hide all org-mode headline property drawers in buffer. Could be slow if it has a lot of overlays."
+    (interactive)
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward
+              "^ *:properties:\n\\( *:.+?:.*\n\\)+ *:end:\n" nil t)
+        (let ((ov_this (make-overlay (match-beginning 0) (match-end 0))))
+          (overlay-put ov_this 'display "")
+          (overlay-put ov_this 'hidden-prop-drawer t))))
+    (put 'org-toggle-properties-hide-state 'state 'hidden))
+
+  (defun org-show-properties ()
+    "Show all org-mode property drawers hidden by org-hide-properties."
+    (interactive)
+    (remove-overlays (point-min) (point-max) 'hidden-prop-drawer t)
+    (put 'org-toggle-properties-hide-state 'state 'shown))
+
+  (defun org-toggle-properties ()
+    "Toggle visibility of property drawers."
+    (interactive)
+    (if (eq (get 'org-toggle-properties-hide-state 'state) 'hidden)
+        (org-show-properties)
+      (org-hide-properties)))
+
+  ;; call org-hide-properties after inserting a new node
+  (add-hook 'org-roam-post-node-insert-hook #'(lambda (_ _) (org-hide-properties))))
 
 (defun get-string-from-file (filePath)
   (with-temp-buffer
