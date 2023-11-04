ab14902730341abe86d48956915de4bae2e67268
Romain Ouabdelkader <romain.ouabdelkader@gmail.com>
2023-11-04 20:26:22 +0100

    compilation: quit window if successfull

--- init.el
+++ init.el
@@ -1532,6 +1532,21 @@ fringe and marginal icons.
   :custom
   (fancy-compilation-override-colors nil))
 
+(defun bury-compile-buffer-if-successful (buffer string)
+  "Bury a compilation buffer if succeeded without warnings "
+  (if (and
+       (string-match "compilation" (buffer-name buffer))
+       (string-match "finished" string)
+       (not
+        (with-current-buffer buffer
+          (goto-char 1)
+          (search-forward "warning" nil t))))
+      (run-with-timer 1 nil
+                      (lambda (buf)
+                        (quit-window nil (get-buffer-window buf)))
+                      buffer)))
+(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
+
 (with-eval-after-load 'compile
   (fancy-compilation-mode))
 
