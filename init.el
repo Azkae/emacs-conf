cac9addc5b746c10e084b41e545d9d9c8dd0967c
Romain Ouabdelkader <romain.ouabdelkader@gmail.com>
2023-12-15 01:20:47 +0100

    improve is-buffer-valid perf

--- init.el
+++ init.el
@@ -273,9 +273,10 @@
 
 (defun is-buffer-valid (buffer-name)
   (let ((valid-buffer t))
-    (cl-loop for boring-buffer in boring-buffers do
-          (when (string-match boring-buffer buffer-name)
-            (setq valid-buffer nil)))
+    (cl-loop for boring-buffer in boring-buffers
+             while valid-buffer do
+             (when (string-match boring-buffer buffer-name)
+               (setq valid-buffer nil)))
     valid-buffer))
 
 (defun conf--skip-temp-buffers (func)
