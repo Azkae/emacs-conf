9e66cace9a52db20b2cf17916ac83077212a465d
Romain Ouabdelkader <romain.ouabdelkader@gmail.com>
2023-12-22 01:25:25 +0100

    remove unused c++ config

--- init.el
+++ init.el
@@ -236,26 +236,6 @@
 
 (setq c-default-style "linux")
 
-(add-hook 'c++-mode-hook
-      #'(lambda()
-        (font-lock-add-keywords
-         nil '(;; complete some fundamental keywords
-           ("\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long\\|float\\|double\\)\\>" . font-lock-keyword-face)
-           ;; add the new C++11 keywords
-           ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . font-lock-keyword-face)
-           ("\\<\\(char[0-9]+_t\\)\\>" . font-lock-keyword-face)
-           ;; PREPROCESSOR_CONSTANT
-           ("\\<[A-Z]+[A-Z_]+\\>" . font-lock-constant-face)
-           ;; hexadecimal numbers
-           ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
-           ;; integer/float/scientific numbers
-	   ("\\<[-+]?[0-9]*\\.?[0-9]*\\([uUlL]+\\|[eE][-+]?[0-9]+\\)?[fFlL]?\\>" . font-lock-constant-face)
-           ;; user-types (customize!)
-           ;; ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(t\\|type\\|ptr\\)\\>" . font-lock-type-face)
-           ;; ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)
-           ))
-        ) t)
-
 (add-to-list 'auto-mode-alist '("\\.mm?\\'" . objc-mode))
 
 ;; avoid boring buffers
