af996926fb653b34a1471e94d9c0c9215a0cdf9b
Romain Ouabdelkader <romain.ouabdelkader@voodoo.io>
2024-06-21 10:37:03 +0200

    fix eval-after-load

--- init.el
+++ init.el
@@ -1514,7 +1514,7 @@ length override, set to t for manual completion."
 
 (which-function-mode)
 
-(eval-after-load 'nxml-mode
+(with-eval-after-load 'nxml-mode
   (modify-syntax-entry ?< "." nxml-mode-syntax-table)
   (modify-syntax-entry ?> "." nxml-mode-syntax-table)
   (modify-syntax-entry ?/ "." nxml-mode-syntax-table))
