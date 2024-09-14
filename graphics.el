ae3dca1ed28b8bd1072e1c5b70aa7353b8686c41
Romain Ouabdelkader <romain.ouabdelkader@voodoo.io>
2024-09-14 11:08:00 +0200

    remove some old graphic settings

--- graphics.el
+++ graphics.el
@@ -11,31 +11,6 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-(defun git-gutter-fringe-plus--set-faces (frame)
-  (with-selected-frame frame
-    (git-gutter-fr+-minimal)
-    (set-face-attribute 'git-gutter-fr+-added    nil :foreground "gray25")
-    (set-face-attribute 'git-gutter-fr+-deleted  nil :foreground "gray25")
-    (set-face-attribute 'git-gutter-fr+-modified nil :foreground "gray25")))
-
-(eval-after-load 'git-gutter-fringe+
-  '(progn
-     (git-gutter-fringe-plus--set-faces (selected-frame))
-     (add-hook 'after-make-frame-functions 'git-gutter-fringe-plus--set-faces)))
-
-
-;; (defun git-gutter-fringe--set-faces (frame)
-;;   (with-selected-frame frame
-;;     (git-gutter-fr+-minimal)
-;;     (set-face-attribute 'git-gutter-fr:added    nil :foreground "gray25" :background nil)
-;;     (set-face-attribute 'git-gutter-fr:deleted  nil :foreground "gray25" :background nil)
-;;     (set-face-attribute 'git-gutter-fr:modified nil :foreground "gray25" :background nil)))
-
-;; (eval-after-load 'git-gutter-fringe
-;;   '(progn
-;;      (git-gutter-fringe--set-faces (selected-frame))
-;;      (add-hook 'after-make-frame-functions 'git-gutter-fringe--set-faces)))
-
 (defun helm--set-faces (frame)
   (with-selected-frame frame
     (set-face-attribute 'helm-ff-directory nil :foreground "Cyan" :weight 'bold
@@ -60,36 +35,8 @@
      (helm--set-faces (selected-frame))
      (add-hook 'after-make-frame-functions 'helm--set-faces)))
 
-
-;; (defun company--set-faces (frame)
-;;   (with-selected-frame frame
-;;     (set-face-attribute 'company-echo-common nil :underline t :foreground nil)
-;;     (set-face-attribute 'company-preview nil :inherit 'shadow :foreground nil :background nil)
-;;     (set-face-attribute 'company-preview-common nil :inherit 'company-preview :underline t :background "LightSteelBlue3" :foreground "dark slate gray")
-;;     (set-face-attribute 'company-scrollbar-bg nil :inherit 'company-tooltip :background "SteelBlue3")
-;;     (set-face-attribute 'company-scrollbar-fg nil :background "DeepSkyBlue4")
-;;     (when (fboundp 'company-template-field)
-;;       (set-face-attribute 'company-template-field nil :background "#49483E" :foreground nil))
-;;     (set-face-attribute 'company-tooltip nil :background "LightSteelBlue1" :foreground "dark slate gray")
-;;     (set-face-attribute 'company-tooltip-annotation nil :inherit 'company-tooltip :foreground "slate gray")
-;;     (set-face-attribute 'company-tooltip-common nil :inherit 'company-tooltip :underline t :foreground nil)
-;;     (set-face-attribute 'company-tooltip-common-selection nil :inherit 'company-tooltip-selection :underline t :foreground nil)
-;;     (set-face-attribute 'company-tooltip-selection nil :inherit 'company-tooltip :background "LightSteelBlue3")
-;;     (set-face-attribute 'company-tooltip-mouse nil :inherit 'company-tooltip :background "LightSteelBlue2")))
-
-;; (eval-after-load 'company
-;;   '(progn
-;;      (company--set-faces (selected-frame))
-;;      (add-hook 'after-make-frame-functions 'company--set-faces)))
-
-
 (defun corfu--set-faces (frame)
   (with-selected-frame frame
-    ;; (set-face-attribute 'corfu-default nil :background "LightSteelBlue1" :foreground "dark slate gray")
-    ;; (set-face-attribute 'completions-common-part nil :inherit 'corfu-default :underline t)
-    ;; (set-face-attribute 'corfu-current nil :inherit 'corfu-default :background "LightSteelBlue3")
-    ;; (set-face-attribute 'corfu-annotations nil :inherit 'corfu-default :background "LightSteelBlue3")
-
     (set-face-attribute 'corfu-border nil :background "#F8F8F2")
     ))
 
