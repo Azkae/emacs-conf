0d1be972981894114efa8a8da404a0c4991c29fc
Romain Ouabdelkader <romain.ouabdelkader@voodoo.io>
2024-12-19 11:49:11 +0100

    better install script

--- install.sh
+++ install.sh
@@ -5,5 +5,11 @@ set -e
 mkdir -p ~/.emacs.d/straight/versions
 ln -fs `pwd`/lock-versions.el ~/.emacs.d/straight/versions/default.el
 
-echo "Put the following in your ~/.emacs:"
-echo "(load-file \"$(pwd)/init.el\")"
+line="(load-file \"$(pwd)/init.el\")"
+
+if ! grep -qF "$line" ~/.emacs; then
+    echo "$line" >> ~/.emacs
+    echo "Added config to ~/.emacs"
+else
+    echo "Config already setup in ~/.emacs, skipping"
+fi
