9731e8b1a9c2cb094982b15978154a4323db70e8
Romain Ouabdelkader <romain.ouabdelkader@voodoo.io>
2025-02-03 16:27:12 +0100

    disable highlight parenthesis

--- init.el
+++ init.el
@@ -2230,9 +2230,9 @@ The thing `string' is not available in Emacs 27.'"
    ("C-c C-l" . crux-downcase-region)
    ("C-c M-c" . crux-capitalize-region)))
 
-(use-package highlight-parentheses
-  :hook
-  (prog-mode . highlight-parentheses-mode))
+;; (use-package highlight-parentheses
+;;   :hook
+;;   (prog-mode . highlight-parentheses-mode))
 
 ;; TODO: test direnv
 
