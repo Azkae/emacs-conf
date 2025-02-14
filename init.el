4d4621eb006ebf568fa3629a9e502ed626a14b1e
Romain Ouabdelkader <romain.ouabdelkader@voodoo.io>
2025-02-14 12:45:32 +0100

    remove fill-column-indicator

--- init.el
+++ init.el
@@ -733,23 +733,6 @@
 
 (use-package glsl-mode)
 
-(use-package fill-column-indicator
-  :config
-  (defvar-local company-fci-mode-on-p nil)
-
-  (defun company-turn-off-fci (&rest ignore)
-    (when (boundp 'fci-mode)
-      (setq company-fci-mode-on-p fci-mode)
-      (when fci-mode (fci-mode -1))))
-
-  (defun company-maybe-turn-on-fci (&rest ignore)
-    (when company-fci-mode-on-p (fci-mode 1)))
-
-  (add-hook 'company-completion-started-hook 'company-turn-off-fci)
-  (add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
-  (add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)
-  (setq fci-rule-column 120))
-
 (use-package jinja2-mode)
 
 (use-package nhexl-mode)
