3612a847c6a2b1d91b262348deb7b011f6e3ce20
Romain Ouabdelkader <romain.ouabdelkader@voodoo.io>
2025-11-22 12:39:06 +0100

    remove treesit-auto

--- init.el
+++ init.el
@@ -1458,12 +1458,47 @@ is a prefix length override, which is t for manual completion."
 ;;     (remove-hook 'projectile-after-switch-project-hook 'conf--poetry-track-virtualenv)))
 ;; (conf--poetry-tracking-mode)
 
-(use-package treesit-auto
-  :custom
-  (treesit-auto-install 'prompt)
+(use-package treesit
+  :straight (:type built-in)
+  :preface
+  (defun my--setup-install-grammars ()
+    "Install Tree-sitter grammars if they are absent."
+    (interactive)
+    (dolist (grammar
+             '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+               (go . ("https://github.com/tree-sitter/tree-sitter-go" "v0.20.0"))
+               (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+               (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1" "src"))
+               (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+               ;; (markdown . ("https://github.com/ikatyang/tree-sitter-markdown" "v0.7.1"))
+               (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+               (rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
+               (toml . ("https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1"))
+               (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+               (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+               ;; (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
+	           ;; (wgsl . ("https://github.com/szebniok/tree-sitter-wgsl" "master"))
+               ))
+      (add-to-list 'treesit-language-source-alist grammar)
+      ;; Only install `grammar' if we don't already have it
+      ;; installed. However, if you want to *update* a grammar then
+      ;; this obviously prevents that from happening.
+      (unless (treesit-language-available-p (car grammar))
+        (treesit-install-language-grammar (car grammar)))))
+  (dolist (mapping
+           '((python-mode . python-ts-mode)
+             (css-mode . css-ts-mode)
+             (typescript-mode . typescript-ts-mode)
+             (js2-mode . js-ts-mode)
+             (bash-mode . bash-ts-mode)
+             (conf-toml-mode . toml-ts-mode)
+             (go-mode . go-ts-mode)
+             (css-mode . css-ts-mode)
+             (json-mode . json-ts-mode)
+             (js-json-mode . json-ts-mode)))
+    (add-to-list 'major-mode-remap-alist mapping))
   :config
-  (setq treesit-auto-langs '(typescript tsx python css javascript))
-  (global-treesit-auto-mode))
+  (my--setup-install-grammars))
 
 (setq-default css-indent-offset 2)
 
