bfb444f22609ec2e95566ca4cf7fd93cfece5368
Romain Ouabdelkader <romain.ouabdelkader@voodoo.io>
2025-11-28 19:40:32 +0100

    magit-delta overlay bug is fixed: disable magit quit

--- init.el
+++ init.el
@@ -692,7 +692,6 @@ Returns nil if there is no active region."
   (magit-list-refs-sortby "-creatordate")
   (magit-diff-visit-avoid-head-blob t)
   (magit-auto-revert-immediately t)
-  (magit-bury-buffer-function (lambda (_) (magit-mode-quit-window t)))
   (vc-display-status nil)
   (magit-diff-visit-prefer-worktree t))
 
